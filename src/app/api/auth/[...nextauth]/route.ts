244351428a7a76fd26e21b2f2659733df4012d5d
hnt-s <hinasai0930@gmail.com>
2024-10-30 11:57:46 +0900

    authentication fixed

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,39 +1,28 @@
 import CredentialsProvider from "next-auth/providers/credentials";
 import { getUserFromDb, connectDB } from "@/utils/db";
-import { signInSchema } from "@/lib/zod"
-import NextAuth from "next-auth"
+import { signInSchema } from "@/lib/zod";
+import NextAuth from "next-auth";
 
 const authOptions = {
-    //providers
-    providers: [
-        //ユーザ用認証
-        CredentialsProvider({
-            name: 'credentials',
-            credentials: {
-                email: { label: 'メールアドレス', type: 'email', placeholder:'xxxx@gmail.com'},
-                password: { label: 'パスワード', type: 'password' }
-            },
-            //認証プログラムを用意
-            async authorize(credentials) {
-                const { email, password } = await signInSchema.parseAsync(credentials);
+  providers: [
+    CredentialsProvider({
+      name: 'credentials',
+      credentials: {
+        email: { label: 'メールアドレス', type: 'email', placeholder: 'xxxx@gmail.com' },
+        password: { label: 'パスワード', type: 'password' }
+      },
+      async authorize(credentials) {
+        const { email, password } = await signInSchema.parseAsync(credentials);
+        await connectDB();
+        const user = await getUserFromDb(email, password);
+        if (!user) {
+          throw new Error("ユーザーが存在しません");
+        }
+        return user;
+      }
+    })
+  ],
+};
 
-                await connectDB();
-
-                // データベースからユーザを取得
-                const user = await getUserFromDb(email, password);
-
-                // ユーザーが存在しない場合
-                if (!user) {
-                    throw new Error("ユーザーが存在しません");
-                }
-
-                return user;
-            }
-        })
-    ],
-
-}
-
-const handler = NextAuth(authOptions)
-
-export {handler as GET, handler as POST }
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
