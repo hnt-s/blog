7b5b3d585896bd54b9bb9a4386bec068583f1294
hnt-s <hinasai0930@gmail.com>
2024-11-30 23:18:34 +0900

    marakdown file deleted

--- src/app/api/markdown/preview.ts
+++
@@ -1,9 +0,0 @@
-// /app/api/markdown/preview.ts
-import { NextResponse } from 'next/server';
-import markdownToHtml from 'zenn-markdown-html';
-
-export async function POST(request: Request) {
-    const { markdownText } = await request.json();
-    const htmlContent = markdownToHtml(markdownText);
-    return NextResponse.json({ htmlContent });
-}
